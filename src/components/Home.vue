c08e9a94dffda7176abbce15e3c88699b1a57d4f
netomatiazi <netoomn@gmail.com>
2020-09-14 15:49:15 -0300

    Up the method for processing subtitles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,18 @@
 <template>
   <v-container fluid>
+      <v-form>
+          <!-- Multiple recebe mais de um arquivo -->
+          <v-file-input 
+            multiple 
+            chips 
+            v-model="files" 
+            prepend-icon="mdi-message-text" 
+            append-outer-icon="mdi-send"
+            label="Selecione as Legendas" 
+            outlined 
+            @click:append-outer="processSubtitles"
+            />
+      </v-form>
     <div class="pills">
           <Pill v-for="word in groupedWords" :key="word.name"
             :name="word.name" :amount="word.amount" />
@@ -13,11 +26,18 @@ export default {
     components: { Pill },
     data: function () {
         return {
+            files:[],
             groupedWords:[
                 {name:'i', amount: 1234},
                 {name:'you', amount: 950},
                 {name:'he', amount: 820},
             ]
+        }
+    },
+    methods: {
+        //Processando a legenda
+        processSubtitles(){
+            console.log(this.files)
         }
     }
 }
